73a08e47e7a71f07eecc2dbb933a4713ce42628f
Warbacon <joaquinguerratocino@gmail.com>
2023-12-11 12:15:02 +0100

    Fix pylint

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/bin/python3
 
-import subprocess
 import os
+import subprocess
+import sys
 
 profiles: dict = {1: "Gnome", 2: "KDE Plasma (BETA)"}
 
@@ -11,51 +12,51 @@ for profile_number, profile_name in profiles.items():
 
 # Profile selection
 selection: str = ""
-profile: int = 0
+PROFILE: int = 0
 
-while profile == 0:
+while PROFILE == 0:
     print('Select a profile (enter "c" to cancel):', end=" ")
     selection: str = input()
 
     if selection.isdigit() and int(selection) in profiles:
-        profile = int(selection)
+        PROFILE = int(selection)
     else:
         if selection == "c":
-            exit(1)
+            sys.exit(1)
         else:
             print("Enter a valid number [1-2]")
 
 
 packages: list = []
-profile_file: str = ""
-display_manager: str = ""
-match profile:
+PROFILE_FILE: str = ""
+DISPLAY_MANAGER: str = ""
+match PROFILE:
     case 1:
         print(f"You selected {profiles[1]}")
-        profile_file = "gnome_packages.txt"
-        display_manager = "gdm"
+        PROFILE_FILE = "gnome_packages.txt"
+        DISPLAY_MANAGER = "gdm"
     case 2:
         print(f"You selected {profiles[2]}")
-        profile_file = "plasma_packages.txt"
-        display_manager = "sddm"
+        PROFILE_FILE = "plasma_packages.txt"
+        DISPLAY_MANAGER = "sddm"
     case _:
         print("An error has ocurred")
 
 # Read packages from profile_file
-if os.path.exists(profile_file):
-    with open(profile_file) as f:
+if os.path.exists(PROFILE_FILE):
+    with open(PROFILE_FILE, encoding="utf8") as f:
         for line in f:
             packages.append(line.rstrip())
 else:
-    print(f"File {profile_file} does not exist")
-    exit(1)
+    print(f"File {PROFILE_FILE} does not exist")
+    sys.exit(1)
 
 # Install packages
-if subprocess.run(["sudo", "pacman", "-S", *packages, display_manager]).returncode != 0:
-    exit(1)
+if subprocess.run(["sudo", "pacman", "-S", *packages, DISPLAY_MANAGER]).returncode != 0:
+    sys.exit(1)
 
 # Enable display manager
-subprocess.run(["sudo", "systemctl", "enable", display_manager])
+subprocess.run(["sudo", "systemctl", "enable", DISPLAY_MANAGER])
 
 # Configure virtual machine
 virt_system: str = subprocess.run(
@@ -63,17 +64,17 @@ virt_system: str = subprocess.run(
 ).stdout.decode()
 
 virt_packages: list = []
-virt_service: str = ""
+VIRT_SERVICE: str = ""
 match virt_system:
     case "vmware":
         virt_packages = ["open-vm-tools", "gtkmm3"]
-        virt_service = "vmtoolsd.service"
+        VIRT_SERVICE = "vmtoolsd.service"
     case "oracle":
         virt_packages = ["virtualbox-guest-utils"]
-        virt_service = "vboxservice.service"
+        VIRT_SERVICE = "vboxservice.service"
 
 if subprocess.run(["sudo", "pacman", "-S", *virt_packages]).returncode == 0:
-    subprocess.run(["sudo", "systemctl", "enable", virt_service])
+    subprocess.run(["sudo", "systemctl", "enable", VIRT_SERVICE])
 
 # Baconize :)
 print("Baconize? [y/N]:", end=" ")
